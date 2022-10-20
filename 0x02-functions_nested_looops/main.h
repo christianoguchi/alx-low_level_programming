0067a1c4c885c0e5b8c07772594fba19abce543f
christianoguchi <chinexguchi@gmail.com>
2022-10-20 02:07:45 -0700

    main.h

--- 0x02-functions_nested_looops/main.h
+++ 0x02-functions_nested_looops/main.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,6 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
+
 
 #endif
